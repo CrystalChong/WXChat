c75c895ad7dfa11793b889cf9d14c72783520f52
Sunfairy <zhangchong@jgntech.com>
2017-07-14 16:54:10 +0800

    这次在我的电脑上修改了

--- WXChat/AppDelegate.m
+++ WXChat/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@"在我的电脑上");
     return YES;
 }
 
